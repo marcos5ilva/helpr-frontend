b32a2d85211d755f175dd22033c65da5ae46b047
Marcos Silva <marcos.silva79@gmail.com>
2020-04-18 21:04:24 -0400

    Added application buid

---
+++ build/precache-manifest.291f9bf9d2afc326dc78d3c666b8744b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a4c794e6ff8dfee53afc1d263333af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae51ac457bafdd9e82e7",
+    "url": "/static/css/main.13016201.chunk.css"
+  },
+  {
+    "revision": "3de389a8ea4e175d4551",
+    "url": "/static/js/2.fbde2239.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fbde2239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae51ac457bafdd9e82e7",
+    "url": "/static/js/main.ba8f3673.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "4e9cbc650eeb146899ca7d16104ed8c6",
+    "url": "/static/media/heroes.4e9cbc65.png"
+  },
+  {
+    "revision": "762b3ee6962e3a89158406241d6a8e15",
+    "url": "/static/media/logo.762b3ee6.svg"
+  }
+]);
